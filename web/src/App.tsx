434ed81c3c1d4d183f983c9bdc64663d7d2fda6b
Murillo Mendes <murillomendes95@hotmail.com>
2022-11-12 16:37:34 -0300

    feat: added react router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,26 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Login } from './views/Login';
+import { Home } from './views/Home';
+import { NotFound } from './views/NotFound';
+import { Layout } from './components/Layout';
+import { Profile } from './views/Profile';
+
 function App() {
-    return <div>Hello world</div>;
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/login">Login</Link>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="profile" element={<Profile />} />
+                </Route>
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
