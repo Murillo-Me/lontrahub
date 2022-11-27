606430dc73faa5b3fd85c8c5e1be02300cac67dc
Murillo Mendes <murillomendes95@hotmail.com>
2022-11-26 23:20:26 -0300

    chore: prisma ORM setup and basic user/post entity

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UserController, UserService } from './user';
+import { PostController, PostService } from './post';
+
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, PostController],
+  providers: [PrismaService, AppService, UserService, PostService],
 })
 export class AppModule {}
--- web/src/views/Login/index.tsx
+++ web/src/views/Login/index.tsx
@@ -7,9 +7,9 @@ console.log('image', BackgroundImage);
 
 export function Login() {
     return (
-        <div className="h-screen">
+        <div className="h-screen bg-darkteal">
             <PublicHeader />
-            <div className="h-[calc(100%-theme(space.header-height))] flex justify-between items-center bg-hero">
+            <div className="h-[calc(100%-theme(space.header-height))] w-full flex justify-between items-center bg-hero bg-center bg-no-repeat bg-cover">
                 <div className="mr-20 ml-auto">
                     <LoginForm />
                 </div>
